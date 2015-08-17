c2d5b875db49bada58374957350cb4066f4030b1
Adrian Wong <adrianw1832@hotmail.co.uk>
2015-08-17 15:59:24 +0100

    Finished Stage 2

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -52,78 +52,62 @@ describe('Thermostat', function() {
       expect(thermostat.powerSavingMode).toBe(true)
     });
 
-    it('has a maximum of 25', function() {
+    it('has a maximum of 25 with power saving mode on', function() {
       while(thermostat.temperature < 25) {
         thermostat.increaseTemperature();
       };
       thermostat.increaseTemperature();
       expect(thermostat.temperature).toEqual(25);
     });
+
+    it('has a maximum of 32 with power saving mode off', function() {
+      thermostat.powerSaveToggle();
+      console.log(thermostat.powerSavingMode)
+      while(thermostat.temperature < 32) {
+        thermostat.increaseTemperature();
+      };
+      thermostat.increaseTemperature();
+      expect(thermostat.temperature).toEqual(32);
+    });
+  });
+
+  describe('display colour', function() {
+    it('shows yellow by default', function() {
+      expect(thermostat.displayColour).toEqual('Yellow');
+    });
+
+    it('shows green when temperature is less than 18', function() {
+      while(thermostat.temperature > 17) {
+        thermostat.decreaseTemperature();
+      };
+      thermostat.displayUpdate();
+      expect(thermostat.displayColour).toEqual('Green');
+    });
+
+    it('shows yellow when temperature is less than 25', function() {
+      while(thermostat.temperature < 24) {
+        thermostat.increaseTemperature();
+      };
+      thermostat.displayUpdate();
+      expect(thermostat.displayColour).toEqual('Yellow');
+    });
+
+    it('shows red when temperature is greater than 24', function() {
+      while(thermostat.temperature < 25) {
+        thermostat.increaseTemperature();
+      };
+      thermostat.displayUpdate();
+      expect(thermostat.displayColour).toEqual('Red');
+    });
+
+    it('is able to change back to yellow', function() {
+      while(thermostat.temperature < 25) {
+        thermostat.increaseTemperature();
+      };
+      thermostat.displayUpdate();
+      thermostat.decreaseTemperature();
+      thermostat.displayUpdate();
+      expect(thermostat.displayColour).toEqual('Yellow');
+    });
   });
 });
-
-
-
-// describe("Player", function() {
-//   var player;
-//   var song;
-
-//   beforeEach(function() {
-//     player = new Player();
-//     song = new Song();
-//   });
-
-
-//   it('', function(){
-
-//   });
-
-//   it("should be able to play a Song", function() {
-//     player.play(song);
-//     expect(player.currentlyPlayingSong).toEqual(song);
-
-//     //demonstrates use of custom matcher
-//     expect(player).toBePlaying(song);
-//   });
-
-//   describe("when song has been paused", function() {
-//     beforeEach(function() {
-//       player.play(song);
-//       player.pause();
-//     });
-
-//     it("should indicate that the song is currently paused", function() {
-//       expect(player.isPlaying).toBeFalsy();
-
-//       // demonstrates use of 'not' with a custom matcher
-//       expect(player).not.toBePlaying(song);
-//     });
-
-//     it("should be possible to resume", function() {
-//       player.resume();
-//       expect(player.isPlaying).toBeTruthy();
-//       expect(player.currentlyPlayingSong).toEqual(song);
-//     });
-//   });
-
-//   // demonstrates use of spies to intercept and test method calls
-//   it("tells the current song if the user has made it a favorite", function() {
-//     spyOn(song, 'persistFavoriteStatus');
-
-//     player.play(song);
-//     player.makeFavorite();
-
-//     expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-//   });
-
-//   //demonstrates use of expected exceptions
-//   describe("#resume", function() {
-//     it("should throw an exception if song is already playing", function() {
-//       player.play(song);
-
-//       expect(function() {
-//         player.resume();
-//       }).toThrowError("song is already playing");
-//     });
-//   });
-// });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,18 +1,21 @@
 function Thermostat() {
   this.temperature = 20;
   this.powerSavingMode = true;
+  this.displayColour = 'Yellow';
 };
 
 Thermostat.prototype.increaseTemperature = function() {
-    if(this.powerSavingMode === true && this.temperature < 25) {
-      this.temperature++;
-    };
+  if(this.powerSavingMode === true && this.temperature < 25) {
+    this.temperature++;
+  } else if (this.powerSavingMode === false && this.temperature < 32) {
+    this.temperature++;
+  };
 };
 
 Thermostat.prototype.decreaseTemperature = function() {
-    if(this.temperature > 10 ) {
-      this.temperature--;
-    };
+  if(this.temperature > 10 ) {
+    this.temperature--;
+  };
 };
 
 Thermostat.prototype.resetTemperature = function() {
@@ -20,34 +23,16 @@ Thermostat.prototype.resetTemperature = function() {
 };
 
 Thermostat.prototype.powerSaveToggle = function() {
-  if(this.powerSavingMode === true) {
-    this.powerSavingMode = false;
+  this.powerSavingMode = (this.powerSavingMode !== true);
+};
+
+Thermostat.prototype.displayUpdate = function() {
+  if(this.temperature < 18) {
+    this.displayColour = 'Green';
+  } else if(this.temperature > 24) {
+    this.displayColour = 'Red';
   } else {
-    this.powerSavingMode = true;
+    this.displayColour = 'Yellow';
   };
 };
 
-// function Player() {
-// }
-
-// var FizzBuzz = function
-// Player.prototype.play = function(song) {
-//   this.currentlyPlayingSong = song;
-//   this.isPlaying = true;
-// };
-
-// Player.prototype.pause = function() {
-//   this.isPlaying = false;
-// };
-
-// Player.prototype.resume = function() {
-//   if (this.isPlaying) {
-//     throw new Error("song is already playing");
-//   }
-
-//   this.isPlaying = true;
-// };
-
-// Player.prototype.makeFavorite = function() {
-//   this.currentlyPlayingSong.persistFavoriteStatus(true);
-// };
